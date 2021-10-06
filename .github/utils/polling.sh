5b8dac6bb76a2b3eaf13f513635ec30bb3a4cda0
Matthias Kautzner <37363333+makautzn@users.noreply.github.com>
2021-10-06 16:25:33 +0200

    Update polling.sh

--- .github/utils/polling.sh
+++ .github/utils/polling.sh
@@ -1,68 +1,65 @@
-name: POI RELEASE
+#!/bin/bash
 
-on:
-  workflow_dispatch:
-  push:
-    branches: 
-    - main
-    paths:
-    - 'apis/poi/**'
-    - '.github/workflows/poi-release.yml'
+declare -i duration=1
+declare hasUrl=""
+declare endpoint
 
-env:
-  APPLICATIONPATH: apis/poi/
-  APPLICATIONNAME: openhackqug903g3poi
-  REGISTRY: ghcr.io
-  IMAGE_NAME: ${{ github.repository }}/poi
-  
-jobs:
-  build-and-push-image:
-    runs-on: ubuntu-latest
-    permissions:
-      contents: read
-      packages: write
-
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v2
-
-      - name: Log in to the Container registry
-        uses: docker/login-action@f054a8b539a109f9f41c372932f1ae047eff08c9
-        with:
-          registry: ${{ env.REGISTRY }}
-          username: ${{ github.actor }}
-          password: ${{ secrets.GITHUB_TOKEN }}
-
-      - name: Build and push Docker image
-        uses: docker/build-push-action@ad44023a93711e3deb337508980b4b5e9bcdc5dc
-        with:
-          context: ${{ env.APPLICATIONPATH }}/web
-          push: true
-          tags: ${{ env.REGISTRY }}/${{ env.IMAGE_NAME }}:${{ github.run_number }}
-
-  deploy:
-    needs: build-and-push-image
-    runs-on: ubuntu-latest
-    steps:
-    - name: 'Login via Azure CLI'
-      uses: azure/login@v1
-      with:
-        creds: ${{ secrets.AZURE_CREDENTIALS }}
+usage() {
+    cat <<END
+    polling.sh [-i] [-h] endpoint
     
-    #- uses: azure/docker-login@v1
-      #with:
-      #  login-server: mycontainer.azurecr.io
-      #  username: ${{ secrets.REGISTRY_USERNAME }}
-      #  password: ${{ secrets.REGISTRY_PASSWORD }}
-    #- run: |
-    #    docker build . -t mycontainer.azurecr.io/myapp:${{ github.sha }}
-    #    docker push mycontainer.azurecr.io/myapp:${{ github.sha }}     
-      
-    - uses: azure/webapps-deploy@v2
-      with:
-        app-name: ${{ env.APPLICATIONNAME }}
-        images: ${{ env.REGISTRY }}/${{ env.IMAGE_NAME }}:${{ github.run_number }}
-    
-    - name: Azure logout
-      run: |
-        az logout
+    Report the health status of the endpoint
+    -i: include Uri for the format
+    -h: help
+END
+}
+
+while getopts "ih" opt; do 
+  case $opt in 
+    i)
+      hasUrl=true
+      ;;
+    h) 
+      usage
+      exit 0
+      ;;
+    \?)
+     echo "Unknown option: -${OPTARG}" >&2
+     exit 1
+     ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+if [[ $1 ]]; then
+  endpoint=$1
+else
+  echo "Please specify the endpoint."
+  usage
+  exit 1 
+fi 
+
+
+healthcheck() {
+    declare url=$1
+    result=$(curl --http2 -i $url 2>/dev/null | grep "HTTP/[12][12\.]*")
+    echo $result
+}
+
+while [[ true ]]; do
+   result=`healthcheck $endpoint` 
+   declare status
+   if [[ -z $result ]]; then 
+      status="N/A"
+   else
+      status=${result:9:3}
+   fi 
+   timestamp=$(date "+%Y%m%d-%H%M%S")
+   if [[ -z $hasUrl ]]; then
+     echo "$timestamp | $status "
+   else
+     echo "$timestamp | $status | $endpoint " 
+   fi 
+   sleep $duration
+done
